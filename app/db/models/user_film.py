1551e8c88691bbd2011f96145c28142016d927d4
SamSteve09 <stevemulyono.sm@gmail.com>
2025-04-15 21:58:00 +0700

    Add user_film model

---
+++ app/db/models/user_film.py
@@ -0,0 +1,19 @@
+from sqlmodel import Field, SQLModel
+from uuid import uuid4, UUID
+from sqlalchemy import Text
+from datetime import date
+from enum import Enum
+
+class UserFilmStatus(str, Enum):
+    PLAN_TO_WATCH = "plan_to_watch"
+    WATCHING = "watching"
+    COMPLETED = "completed"
+    ON_HOLD = "on_hold"
+    DROPPED = "dropped"
+
+class UserFilmModel(SQLModel,table = True):
+    id: int = Field(default_factory=uuid4, primary_key=True)
+    status: UserFilmStatus = Field()
+    user_id: UUID = Field(foreign_key="usermodel.id")
+    film_id: UUID = Field(foreign_key="filmmodel.id")
+    progress: int = Field(default=0)
