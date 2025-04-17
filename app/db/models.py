7d9bfafe2cbc2feac6d9180fa0611cdb67aa9d56
SamSteve09 <stevemulyono.sm@gmail.com>
2025-04-17 19:18:21 +0700

    Add on delete cascade and nullable attribute

--- app/db/models.py
+++ app/db/models.py
@@ -5,26 +5,27 @@ from typing import Text
 from enums import FilmStatus, Role, UserFilmStatus, ReactionType
 
 class GenreFilm(SQLModel,table = True):
-    film_id: UUID = Field(default_factory=uuid4, primary_key=True, foreign_key="film.id")
-    genre_id: int | None = Field(default_factory=uuid4, primary_key=True, foreign_key="genre.id")
+    film_id: UUID = Field(primary_key=True, foreign_key="film.id",cascade_delete=True)
+    genre_id: int | None = Field(default=None,primary_key=True, foreign_key="genre.id",cascade_delete=True)
     
 class UserFilm(SQLModel,table = True):
     id: int | None = Field(default=None,primary_key=True)
     status: UserFilmStatus
-    user_id: UUID = Field(foreign_key="user.id")
-    film_id: UUID = Field(foreign_key="film.id")
+    user_id: UUID = Field(default_factory=uuid4, foreign_key="user.id", cascade_delete=True)
+    film_id: UUID = Field(default_factory=uuid4, foreign_key="film.id", cascade_delete=True)
     progress: int = Field(default=0)
     
 class Genre(SQLModel,table = True):
     id: int = Field(default=None, primary_key=True)
     genre_name: str = Field(min_length=1,max_length=255,unique=True)
+    
     films: list["Film"] = Relationship(back_populates="genres", link_model=GenreFilm)
     
 class User(SQLModel,table = True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     username: str = Field(min_length=1,max_length=255,unique=True,index=True)
     password_hash: str
-    bio: str = Field(nullable=True,sa_column_kwargs={"type_": Text})
+    bio: str | None = Field(default=None,nullable=True,sa_column_kwargs={"type_": Text})
     is_private: bool = Field(default=False)
     created_at: date = Field(default=date.today())
     role: Role = Field(default=Role.USER)
@@ -34,36 +35,37 @@ class User(SQLModel,table = True):
 class Film(SQLModel,table = True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     title: str = Field(min_length=1,max_length=255)
-    synopsis: str = Field(nullable=True,sa_column_kwargs={"type_": Text})
-    release_date: date = Field(nullable=True)
+    synopsis: str | None = Field(sa_column_kwargs={"type_": Text})
+    release_date: date | None = Field(default=None, nullable=True)
     air_status: FilmStatus
-    episode_count: int = Field(default=1,nullable=True)
-    rating: float = Field(default=0,nullable=True)
-    rating_count: int = Field(default=0,nullable=True)
+    episode_count: int | None = Field(default=None, nullable=True)
+    rating: float | None = Field(default=None, ge=0, le=10, nullable=True)
+    rating_count: int | None = Field(default=0, nullable=True)
     
     genres: list["Genre"] = Relationship(back_populates="films", link_model=GenreFilm)
     users: list["User"] = Relationship(back_populates="films", link_model=UserFilm)
 
 class Reaction(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user_id: UUID = Field(foreign_key="user.id")
-    review_id: UUID = Field(foreign_key="review.id")
+    user_id: UUID = Field(foreign_key="user.id") # reaction from a deleted user won't go away
+    review_id: UUID = Field(foreign_key="review.id", cascade_delete=True)
     reaction_type: ReactionType = Field()
 
-    review: "Review" = Relationship(back_populates="reactions")
+    review: "Review" = Relationship(back_populates="reaction")
     
 class Review(SQLModel,table = True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
-    user_film_id: int = Field(foreign_key="userfilm.id")
+    # delete review if film is deleted from user list (implicitly delete if either film or user is also deleted)
+    user_film_id: int = Field(foreign_key="userfilm.id",cascade_delete=True)
     rating: int = Field(ge=1, le=10)
-    comment: str = Field(nullable=True,sa_column_kwargs={"type_": Text})
+    comment: str | None = Field(sa_column_kwargs={"type_": Text})
     like_count: int = Field(default=0)
     dislike_count: int = Field(default=0)
     created_at: datetime = Field(default_factory=datetime.now)
-    last_updated_at: datetime = Field(nullable=True)
+    last_updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
     
-    user_film: UserFilm = Relationship(back_populates="reviews")
-    reactions: list["Reaction"] = Relationship(back_populates="reviews")
+    #user_films: "UserFilm" = Relationship(back_populates="reviews")
+    reactions: list["Reaction"] = Relationship(back_populates="review")
     
 
     
